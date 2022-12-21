4376a720aee07428b18be36a6e1d69e06d63f302
Fithamlak <justiceofgod01@gmail.com>
2022-12-21 17:06:12 +0300

    creat array of objects for my accomblishments or works with one element

--- index.js
+++ index.js
@@ -27,3 +27,18 @@ function closeNav() {
 hamburgerButton.addEventListener('click', openNav);
 closeButton.addEventListener('click', closeNav);
 navItems.addEventListener('click', closeNav);
+
+const accomplishmentArray = [
+  {
+    id: 0,
+    title: 'Tonic',
+    experiences: ['Canopy', 'Back End Dev', '2015'],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    technologies: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'],
+    ImageForMobile: '/images/first-work.svg',
+    ImageForDesktop: '/images/work1-desktop-view.svg',
+    alt: 'work_tonic_daily_personalized_reads',
+    live: 'https://fithamlak.github.io/',
+    source: 'https://github.com/fithamlak/My-portfolio',
+  },
+];
