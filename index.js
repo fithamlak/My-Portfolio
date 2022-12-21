80a512e9e09b336f3041cc8ff7c0cd3df802dc74
Fithamlak <justiceofgod01@gmail.com>
2022-12-21 11:28:26 +0300

    check for any linter error

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const nav = document.querySelector("#toolbar");
-const hamburgerButton = document.querySelector(".hamberger");
-const closeButton = document.querySelector(".close-button");
-const navItems = document.querySelector(".menu-list");
-const myLogo = document.querySelector(".logo");
+const nav = document.querySelector('#toolbar');
+const hamburgerButton = document.querySelector('.hamberger');
+const closeButton = document.querySelector('.close-button');
+const navItems = document.querySelector('.menu-list');
+const myLogo = document.querySelector('logo');
 
 function openNav() {
-  nav.classList.remove("#toolbar");
-  nav.classList.add("header-bg-color");
-  hamburgerButton.classList.toggle("invisible");
-  myLogo.classList.toggle("invisible");
-  closeButton.classList.toggle("invisible");
-  navItems.classList.toggle("invisible");
-  navItems.classList.toggle("open_menu");
+  nav.classList.remove('#toolbar');
+  nav.classList.add('header-bg-color');
+  hamburgerButton.classList.toggle('invisible');
+  myLogo.classList.toggle('invisible');
+  closeButton.classList.toggle('invisible');
+  navItems.classList.toggle('invisible');
+  navItems.classList.toggle('pen_menu');
 }
 
 function closeNav() {
-  nav.classList.remove("header-bg-color");
-  nav.classList.add("toolbar");
-  hamburgerButton.classList.toggle("invisible");
-  myLogo.classList.toggle("invisible");
-  closeButton.classList.toggle("invisible");
-  navItems.classList.toggle("invisible");
-  navItems.classList.toggle("open_menu");
+  nav.classList.remove('.header-bg-color');
+  nav.classList.add('toolbar');
+  hamburgerButton.classList.toggle('invisible');
+  myLogo.classList.toggle('invisible');
+  closeButton.classList.toggle('invisible');
+  navItems.classList.toggle('invisible');
+  navItems.classList.toggle('open_menu');
 }
 
-hamburgerButton.addEventListener("click", openNav);
-closeButton.addEventListener("click", closeNav);
-navItems.addEventListener("click", closeNav);
+hamburgerButton.addEventListener('click', openNav);
+closeButton.addEventListener('click', closeNav);
+navItems.addEventListener('click', closeNav);
