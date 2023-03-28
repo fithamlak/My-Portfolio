4110d155fe782365e888237080a220d516b31058
Fithamlak <justiceofgod01@gmail.com>
2023-03-28 07:40:44 +0300

    put javascript code in app.js

--- javascript/app.js
+++ javascript/app.js
@@ -23,6 +23,104 @@ function closeMenuHandler() {
 menuBars.addEventListener('click', openMenuHandler);
 closeMenuBtn.addEventListener('click', closeMenuHandler);
 mobileLinks.addEventListener('click', closeMenuHandler);
+const contactForm = document.getElementById('contact-form');
+const {
+  name: nameInput,
+  email: emailInput,
+  message: messageInput,
+} = contactForm.elements;
+const form = document.getElementById('contact-form');
+const { email } = form.elements;
+const alert = document.getElementById('alert');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (email.value === email.value.toLowerCase()) {
+    form.submit();
+    form.reset();
+  } else {
+    alert.classList.replace('d-none', 'd-flex');
+    alert.textContent = 'Email must be lowercase - form not submitted!';
+  }
+});
+
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException
+      // everything except Firefox
+      && (e.code === 22
+        // Firefox
+        || e.code === 1014
+        // test name field too, because code might not be present
+        // everything except Firefox
+        || e.name === 'QuotaExceededError'
+        // Firefox
+        || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
+      // acknowledge QuotaExceededError only if there's something already stored
+      && storage
+      && storage.length !== 0
+    );
+  }
+}
+
+let availableStorage;
+
+if (storageAvailable('localStorage')) {
+  // Yippee! We can use localStorage awesomeness
+  availableStorage = window.localStorage;
+} else {
+  // Too bad, no localStorage for us
+  availableStorage = null;
+}
+
+const formData = {};
+
+function storeData() {
+  formData.name = nameInput.value;
+  formData.email = emailInput.value;
+  formData.message = messageInput.value;
+  const jsonData = JSON.stringify(formData);
+  availableStorage.setItem('contactFormData', jsonData);
+}
+
+nameInput.addEventListener('change', () => {
+  storeData();
+});
+
+emailInput.addEventListener('change', () => {
+  storeData();
+});
+
+messageInput.addEventListener('change', () => {
+  storeData();
+});
+
+function retrieveData() {
+  const data = availableStorage.getItem('contactFormData');
+  const parseData = JSON.parse(data);
+  if (data?.length > 0) {
+    const { name, email, message } = parseData;
+    nameInput.value = name || '';
+    emailInput.value = email || '';
+    messageInput.value = message || '';
+  }
+}
+
+const btnReset = document.getElementById('btn-reset');
+
+btnReset.addEventListener('click', (event) => {
+  event.preventDefault();
+  contactForm.reset();
+  availableStorage.clear();
+});
 
 const worksArray = [
   {
@@ -211,6 +309,7 @@ const worksActivateLinkHandler = () => {
 };
 
 window.onload = () => {
+  retrieveData();
   worksLoadHandler();
   worksActivateLinkHandler();
 };
